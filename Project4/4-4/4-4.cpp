d58fbd543f43f03f0db48cead7f750b0cd946515
suyeon428 <dsy105673@naver.com>
2025-03-27 17:01:19 +0900

    test

--- Project4/4-4/4-4.cpp
+++ Project4/4-4/4-4.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 using namespace std;
 
-class Sample {
-	int* p;
-	int size;
-public:
-	Sample(int n) {
-		size = n; p = new int[n];
-	}
-	void read();
-	void write();
-	int big();
-	~Sample();
-};
+//class Sample {
+//	int* p;
+//	int size;
+//public:
+//	Sample(int n) {
+//		size = n; p = new int[n];
+//	}
+//	void read();
+//	void write();
+//	int big();
+//	~Sample();
+//};
 
 int main() {
+	cout << "test";
 	
 }
